e36d16652e3a0f1ad1912263462477e31d8137c5
Ye Yuan <yuanye_ptr@qq.com>
2022-01-07 00:40:40 +0800

    add comparison

--- src/blotto.rs
+++ src/blotto.rs
@@ -2,10 +2,40 @@ pub struct Allocation {
     pub soldiers: Vec<i64>,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum Result {
+    Win,
+    Lose,
+    Draw,
+}
+
 impl Allocation {
     pub fn new(battle_count: usize, soldier_count: usize) -> Self {
         let mut soldiers: Vec<i64> = vec![0; battle_count];
         soldiers[0] = soldier_count as i64;
         return Allocation { soldiers: soldiers };
+    }
+
+    pub fn compare(&self, another: &Allocation) -> Result {
+        assert!(self.soldiers.len() == another.soldiers.len());
+        let mut win = 0;
+        let mut lose = 0;
+        for i in 0..self.soldiers.len() {
+            if self.soldiers[i] > another.soldiers[i] {
+                win += 1;
+            } else if self.soldiers[i] < another.soldiers[i] {
+                lose += 1;
+            }
+        }
+
+        if win == lose {
+            return Result::Draw;
+        }
+
+        if win > lose {
+            return Result::Win;
+        }
+
+        return Result::Lose;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,4 +9,29 @@ mod tests {
         let alloc = blotto::Allocation::new(3, 5);
         assert_eq!(alloc.soldiers, vec![5, 0, 0]);
     }
+
+    #[test]
+    fn test_compare() {
+        let mut alloc1 = blotto::Allocation::new(3, 5);
+        let mut alloc2 = blotto::Allocation::new(3, 5);
+        assert_eq!(alloc1.compare(&alloc2), blotto::Result::Draw);
+
+        alloc1.soldiers[0] = 2;
+        alloc1.soldiers[1] = 2;
+        alloc1.soldiers[2] = 1;
+        // alloc1 lose battle 0, win 1 and 2
+        assert_eq!(alloc1.compare(&alloc2), blotto::Result::Win);
+
+        alloc2.soldiers[0] = 1;
+        alloc2.soldiers[1] = 2;
+        alloc2.soldiers[2] = 2;
+        // alloc1 win battle 0, draw 1 and lose 2
+        assert_eq!(alloc1.compare(&alloc2), blotto::Result::Draw);
+
+        alloc2.soldiers[0] = 3;
+        alloc2.soldiers[1] = 0;
+        alloc2.soldiers[2] = 2;
+        // alloc1 lose battle 0, win 1 and lose 2
+        assert_eq!(alloc1.compare(&alloc2), blotto::Result::Lose);
+    }
 }
